5b63568f31eb4a6a6fcf672418725b239bae74f6
LorenzoTausani <83968253+LorenzoTausani@users.noreply.github.com>
2022-05-17 00:33:02 +0200

    Update RBM.py

--- RBM.py
+++ RBM.py
@@ -436,7 +436,7 @@ class RBM(nn.Module): #nn.Module: Base class for all neural network modules.
 
         return Avg_cost, Std_cost
 
-    def save_model(self, nr_gibbs_htrain=1, nr_gibbs_htest=1, just_filename=0):
+    def save_model(self, nr_gibbs_htrain=1, nr_gibbs_htest=1):
         #lavora con drive
 
         try:
@@ -450,31 +450,27 @@ class RBM(nn.Module): #nn.Module: Base class for all neural network modules.
             h_test_size = 0
 
 
-        filename = 'rbm_train'+ str(self.nr_train_epochs_done)+'_generated_h_train'+str(h_train_size)+'gibbs'+str(nr_gibbs_htrain)+'_generated_h_test'+str(h_test_size)+'gibbs'+str(nr_gibbs_htest)
+        self.filename = 'rbm_train'+ str(self.nr_train_epochs_done)+'_generated_h_train'+str(h_train_size)+'gibbs'+str(nr_gibbs_htrain)+'_generated_h_test'+str(h_test_size)+'gibbs'+str(nr_gibbs_htest)
+
+        object = self
+ 
+
+        from google.colab import drive
+        drive.mount('/content/gdrive')
+
+        save_path = "/content/gdrive/My Drive/"+filename
+
+        try:
+            os.mkdir(save_path)
+        except:
+            print("Folder already found")
+
+        filename = save_path +'/'+ filename + '.pkl'
+
+        with open(filename, 'wb') as outp:  # Overwrites any existing file.
+            pickle.dump(object, outp, pickle.HIGHEST_PROTOCOL)
         
-
-        if just_filename==0:
-
-            object = self
-
-            
-
-            from google.colab import drive
-            drive.mount('/content/gdrive')
-
-            save_path = "/content/gdrive/My Drive/"+filename
-
-            try:
-                os.mkdir(save_path)
-            except:
-                print("Folder already found")
-
-            filename = save_path +'/'+ filename + '.pkl'
-
-            with open(filename, 'wb') as outp:  # Overwrites any existing file.
-                pickle.dump(object, outp, pickle.HIGHEST_PROTOCOL)
         
-        return filename
         
         
 
